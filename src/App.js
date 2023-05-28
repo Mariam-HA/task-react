fd584cd704704f0f46150bdf664b1a427aeac46c
AMAL ALRASDAN <mariam.alrashdan97@gmail.com>
2023-05-28 18:13:56 +0300

    taskreact

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import image1 from "./assets/media/images.jpeg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="contener">
+        <h1 className="title">Cookies and Beyond</h1>
+        <p className="desctiption">where cookie maniacsgather</p>
+        <img src={image1} alt="resturent" className="resturent image"></img>
+      </div>
+    </>
   );
 }
 
