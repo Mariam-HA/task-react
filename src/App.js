3b6b8fe254ea6d868d6df016682fc4469097e7cf
AMAL ALRASDAN <mariam.alrashdan97@gmail.com>
2023-05-29 03:12:48 +0300

    task 2 done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import "./App.css";
-import image1 from "./assets/media/images.jpeg";
+import image1 from "./assets/media/seaFood.jpeg";
+import products from "./products.js";
 
 function App() {
+  const productsList = products.map((product) => {
+    return (
+      <div className="mainDishes">
+        <img src={product.image} alt={product.name}></img>
+        <p>{product.name}</p>
+        <p>{product.price}</p>
+      </div>
+    );
+  });
+
   return (
     <>
       <div className="contener">
-        <h1 className="title">Cookies and Beyond</h1>
-        <p className="desctiption">where cookie maniacsgather</p>
-        <img src={image1} alt="resturent" className="resturent image"></img>
+        <h1>The Coasteal Catch</h1>
+        <h4>
+          A seafood lover's paradise, offering a tempting selection of freshly
+          caught delights from the ocean, expertly prepared with a creative
+          twist that highlights the natural flavors of the sea.
+        </h4>
+        <img src={image1} alt="resturent"></img>
       </div>
+
+      <div className="plapla">{productsList}</div>
     </>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,31 @@
+import image2 from "./assets/media/salmon-fillets.jpeg";
+import image3 from "./assets/media/seafood paella.jpeg";
+import image4 from "./assets/media/lemon butter.webp";
+import image5 from "./assets/media/crispy-shrimp.jpeg";
+
+const products = [
+  {
+    name: "Grilled Lemon Salmon",
+    price: "12.99 KD",
+    image: image2,
+  },
+
+  {
+    name: "Seafood Paella",
+    price: "17.5 KD",
+    image: image3,
+  },
+
+  //   {
+  //     name: "Lemon Butter",
+  //     price: "15.5 KD",
+  //     image: image4,
+  //   },
+  //   {
+  //     name: "Crispy Shrimp",
+  //     price: "15 KD",
+  //     image: image5,
+  //   },
+];
+
+export default products;
